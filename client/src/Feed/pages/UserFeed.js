d4949d41faed35f4d70a677cd51caff851d4a132
Kyle <khartson2017@gmail.com>
2023-01-02 15:38:29 -0500

    added unsplash api search component

--- client/src/Feed/pages/UserFeed.js
+++ client/src/Feed/pages/UserFeed.js
@@ -1,10 +1,26 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import { FeedMenu } from '../components';
+import UnsplashReact, { InsertIntoApplicationUploader } from "unsplash-react"
+import { Image } from 'semantic-ui-react';
 
 function UserFeed() {
+
+  const [imageUrl, setImageUrl] = useState('');
+
   return(
   <>
     <FeedMenu/>
+    {/* <div>{process.env.REACT_APP_VARIABLE}</div> */}
+    {/* <Image src={imageUrl}/> */}
+      {/* <UnsplashReact
+        accessKey={process.env.REACT_APP_UNSPLASH_KEY}
+        applicationName="unsplash_react"
+        Uploader={InsertIntoApplicationUploader}
+        photoRatio={16 / 9}
+        preferredSize={{ width: 800, height: 450 }}
+        onFinishedUploading={(imageUrl) => setImageUrl(imageUrl)}
+      /> */}
+
     <h1> feed </h1>
   </>
   )
